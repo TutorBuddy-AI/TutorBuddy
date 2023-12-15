9f679cdb916567af23f7b7fbebe382c9e94b4934
de_la_verrier <amir.66garaev@gmail.com>
2023-12-14 23:19:39 -0500

    исправил

--- src/utils/generate/translate/translate.py
+++ src/utils/generate/translate/translate.py
@@ -3,7 +3,7 @@ from src.utils.generate import GenerateAI
 import json
 
 
-class Translate:
+class TranslateGenerate:
     """На вход подаётся английский текст, на выходе переведённый текст на родной язык юзера"""
     def __init__(
             self,
@@ -14,22 +14,19 @@ class Translate:
         self.tg_id = tg_id
         self.request_url = "https://api.openai.com/v1/chat/completions"
 
-    async def translate_string(self) -> str:
-        user_info = await user.get_user_info(tg_id=self.tg_id)
-
-        payload = {
-            "model": "gpt-3.5-turbo",
-            "messages": f"Translate it '{self.eng_string}' on {user_info['native_lang']}",
-            "max_tokens": 400,
-        }
-
-        generated_text = await GenerateAI(request_url=self.request_url).send_request(
-            payload=payload)
-
-        print(generated_text)
+    async def translate(self) -> str:
+        payload = await self.get_combined_data()
+        generated_text = await GenerateAI(request_url=self.request_url).send_request(payload)
 
         if generated_text:
             return generated_text["choices"][0]["message"]["content"]
         else:
             return None
 
+    async def get_combine_data(self) -> json:
+        user_info = await UserService().get_user_info(self.tg_id)
+        return {
+            "model": "gpt-3.5-turbo",
+            "messages": f"Translate it '{self.eng_string}' on {user_info['native_lang']}",
+            "max_tokens": 400
+        }
