35bc05162b958b34917d1a8fdcfac6117ad0d069
Abylaikhan <k.abylaikhan04@gmail.com>
2023-12-16 03:58:57 +0600

    Daily news methods fixed

--- src/utils/daily_news/daily_news.py
+++ src/utils/daily_news/daily_news.py
@@ -3,14 +3,17 @@ from src.database.models import DailyNews
 from src.database import session, Transactional
 from src.database.models.enums.daily_news import DailyNewsEnum
 from sqlalchemy import select, delete
+
 from utils.daily_news.daily_news_history import GetUserDailyNewsHistory
 
 
 class DailyNew:
     def __init__(self):
         ...
+
     @Transactional()
-    async def add_news(self, topic: str, message: str, new_types: DailyNewsEnum.NEWS_TYPE__TEXT, path_to_data: str) -> None:
+    async def add_news(self, topic: str, message: str, new_types: DailyNewsEnum.NEWS_TYPE__TEXT,
+                       path_to_data: str) -> None:
         daily_news = DailyNews(
             topic=topic,
             message=message,
@@ -24,13 +27,13 @@ class DailyNew:
             self,
             topic: str,
             limit: int = 25
-    )-> GetUserDailyNewsHistory:
+    ) -> GetUserDailyNewsHistory:
         query = select(DailyNews).where(DailyNews.topic == str(topic)).limit(limit)
         result = await session.execute(query)
 
         result = result.scalars()
 
-        results = [{'topic': row.topic, 'content': row.message} for row in result]
+        results = [{'topic': row.topic, 'content': row.message, 'type': row.type, 'path to data': row.path_to_data} for row in result]
         return results
 
     @Transactional()
