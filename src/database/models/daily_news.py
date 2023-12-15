458c0af7b9df2861f33251a5e3d1e4b0d963144a
Abylaikhan <k.abylaikhan04@gmail.com>
2023-12-15 19:50:37 +0600

    New migration

--- src/database/models/daily_news.py
+++ src/database/models/daily_news.py
@@ -7,15 +7,14 @@ from src.database.models.enums.daily_news import DailyNewsEnum
 #  many-to-many relationship with users
 # to check if user already get the message
 # user, daily_news.id, is_sended (is_read)...
-
-
 class DailyNews(Base, TimestampMixin):
     __tablename__ = "daily_news"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    message = Column(Text, nullable=True)
-    instruction = Column(Unicode, nullable=True)
+    message = Column(String, nullable=True)
     type = Column(Unicode, nullable=False, default=DailyNewsEnum.NEWS_TYPE__TEXT)
+    topic = Column(String, nullable=True)
+    path_to_data = Column(String, nullable=True)
 
     # we can use sqlalchemy-file to work with files in admin panel
     # from sqlalchemy_file import FileField, ImageField
