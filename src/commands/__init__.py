db96b0d247171bd17952ef34cff05e52943a2d4b
Arthur-king18 <arturik060406@gmail.com>
2023-12-19 20:36:59 +0400

    fix bugs

--- src/commands/__init__.py
+++ src/commands/__init__.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from src.commands.menu.go_back_to_chat import dp
 
 from src.commands.form_states import dp
@@ -18,24 +17,5 @@ from src.commands.cacncel_state import dp
 from src.commands.menu.edit_profile import dp
 from src.commands.text_communication import dp
 from src.commands.audio_communication import dp
-=======
-from src.commands.form_states import dp
-
-from src.commands.start import dp
-from src.commands.text_communication import dp
-from src.commands.audio_communication import dp
-from src.commands.choose_speaker import dp
-
-from src.commands.menu.go_back_to_chat import dp
-from src.commands.menu.restart import dp
-from src.commands.menu.edit_profile import dp
-from src.commands.menu.edit_topic import dp
-from src.commands.menu.edit_person import dp
-from src.commands.menu.support import dp
-from src.commands.menu.feedback import dp
-from src.commands.menu.mistakes import dp
-
-from src.commands.cacncel_state import dp
->>>>>>> 308d362 (change commands)
 
 __all__ = ["dp"]
