48aab9088d5baeb44ef498a986149083f55ec4f3
Бурмистров Михаил Сергеевич <burmistrowm@gmail.com>
2023-12-21 04:22:09 +0300

    fixed mistakes

--- src/utils/message_history_mistakes/user_message_mistakes_helper.py
+++ src/utils/message_history_mistakes/user_message_mistakes_helper.py
@@ -1,4 +1,5 @@
-from utils.message_history_mistakes.schemas import MessageMistakesInfo, MessageStateInfo
+from utils.message.schema.message import MessageStateInfo
+from utils.message_history_mistakes.schemas import MessageMistakesInfo
 from aiogram.types import Message
 
 
@@ -6,14 +7,15 @@ class MessageMistakesHelper:
     def __init__(self):
         ...
 
-    async def group_message_mistakes_info(self, state_message_info: MessageStateInfo, message: Message) -> MessageMistakesInfo:
+    async def group_message_mistakes_info(
+            self, state_message_info: MessageStateInfo, message: Message, mistakes: str) -> MessageMistakesInfo:
         user_mistakes_info = {
             "tg_id": str(message.chat.id),
-            "user_message_id": state_message_info.user_message.id,
-            "bot_message_id": state_message_info.bot_message.id,
+            "user_message_id": None,  # ToDo state_message_info.user_message.id,
+            "bot_message_id": None,  # ToDo state_message_info.bot_message.id,
 
-            "message": message.text,
+            "message": mistakes,
             "role": "assistant",
-            "type": state_message_info.type_message
+            "type": ""  # ToDo state_message_info.type_message
         }
         return user_mistakes_info
