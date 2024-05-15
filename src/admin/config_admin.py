4f9618f3914bbd21cf0e7d706d45c1af013869e2
Бурмистров Михаил Сергеевич <burmistrowm@gmail.com>
2024-05-15 11:46:51 +0300

    cors deleted

--- src/admin/config_admin.py
+++ src/admin/config_admin.py
@@ -10,21 +10,6 @@ from config.config import config
 
 app = FastAPI()
 
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
